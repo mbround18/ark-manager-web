edb09d1268b2f6b95b7c5206118444c94868df61
Michael Bruno <michael.bruno1337@gmail.com>
2017-03-05 02:27:24 -0700

    undoing some of my changes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,9 @@
 WORKING_DIR = File.dirname(File.expand_path('..', __FILE__))
 working_directory WORKING_DIR
 
+stderr_path WORKING_DIR + '/log/unicorn.stderr.log'
+stdout_path WORKING_DIR + '/log/unicorn.stderr.log'
+
 worker_processes 1
 
-listen "#{ENV.fetch('LISTENING_IP', '0.0.0.0')}:#{ENV.fetch('PORT', '8080')}"
+listen "#{ENV.fetch('LISTENING_IP', '0.0.0.0')}:#{ENV.fetch('LISTENING_PORT', '8080')}"
