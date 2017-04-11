43a8094507cc7595957efec26b8c8b0dd2e0e436
ImmortaL <denis@ranneft.ru>
2017-04-12 01:39:37 +0300

    allow arkamanger to be not in ~/bin

--- config/environment.rb
+++ config/environment.rb
@@ -6,25 +6,26 @@ require 'logger'
 require 'base64'
 require 'rufus-scheduler'
 
-WORKING_DIR       = File.dirname(File.expand_path('..', __FILE__)) unless defined?(WORKING_DIR)
-EGREP_EXEC        = find_executable 'egrep'
-CURL_EXEC         = find_executable 'curl'
-USER_HOME         = Dir.home unless defined?(USER_HOME)
-RACK_ENV          = ENV.fetch('RACK_ENV', 'development')
-ARK_MANAGER_CLI   = find_executable('arkmanager', "#{USER_HOME}/bin") unless defined?(ARK_MANAGER_CLI)
+WORKING_DIR          = File.dirname(File.expand_path('..', __FILE__)) unless defined?(WORKING_DIR)
+EGREP_EXEC           = find_executable 'egrep'
+CURL_EXEC            = find_executable 'curl'
+USER_HOME            = Dir.home unless defined?(USER_HOME)
+ARK_MANAGER_CLI_PATH = "#{USER_HOME}/bin" unless defined?(ARK_MANAGER_CLI_PATH)
+RACK_ENV             = ENV.fetch('RACK_ENV', 'development')
 
 memcache_port = '11211'
 memcache_address = 'localhost'
 if File.exists?("#{WORKING_DIR}/config/env_config.json")
   hash = Oj.load_file("#{WORKING_DIR}/config/env_config.json", Hash.new)
   hash.each_pair { |key,value|  ENV[key] = value  }
-  memcache_port = hash['port'] || memcache_port
-  memcache_address = hash['address'] || memcache_address
+  memcache_port        = ENV['memcache_port'] || memcache_port
+  memcache_address     = ENV['memcache_address'] || memcache_address
+  ARK_MANAGER_CLI_PATH = ENV['arkmanager_path'] || ARK_MANAGER_CLI_PATH
 end
 
+ARK_MANAGER_CLI = find_executable('arkmanager', ARK_MANAGER_CLI_PATH) unless defined?(ARK_MANAGER_CLI)
+DOMAIN_NAME     = ENV.fetch('DOMAIN_NAME', 'localhost')
 ENV["TZ"] = ENV.fetch("TZ", "Etc/UTC")
-
-DOMAIN_NAME = ENV.fetch('DOMAIN_NAME', 'localhost')
 
 raise 'I was unable to find arkmanager in your path!! please run "bundle exec rake install:server_tools"' unless ARK_MANAGER_CLI
 raise 'I was unable to find memcached!!! please have your system administrator install memcached' unless find_executable('memcached')
