065437d4b945a48cbc4fe9572cb98cd4c4a4e8b4
Neko Boi Nick <thakyz@outlook.com>
2019-08-09 15:52:00 -0700

    Added config options for port and address

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -15,11 +15,12 @@ address = '0.0.0.0'
 env_config_path = "#{WORKING_DIR}/config/env_config.json"
 
 if File.exist?(env_config_path)
-	file = File.read(env_config_path)
-	hash = JSON.parse!(file, symbolize_names: true)
-  # hash = Oj.load_file("#{WORKING_DIR}/config/env_config.json", Hash.new)
-  port = hash[:port] || port
-  address = hash[:address] || address
+  file = File.read(env_config_path)
+  hash = JSON.parse!(file, symbolize_names: true)
+  hash.each_pair {|key, value| ENV[key.to_s] = value.to_s}
 end
+
+port = ENV.fetch('PORT', port)
+address = ENV.fetch('ADDRESS', address)
 
 listen format('%s:%s', address, port)
